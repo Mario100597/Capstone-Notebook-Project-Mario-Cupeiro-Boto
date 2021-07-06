1ca007710da2e1cdc2dcb81a42ba662fe52723f5
Mario100597 <69730755+Mario100597@users.noreply.github.com>
2021-07-06 18:29:01 +0200

    Add files via upload

---
+++ First final project 10th course.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This  notebook will be specifically used for the final capstone project
+
+# In[3]:
+
+
+import pandas as pd
+
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
